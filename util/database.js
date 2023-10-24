91d75e275918ea18564baa32d6196c27cc52b731
Alamsahithi <sahithialam13@gmail.com>
2023-10-24 14:51:57 +0530

    until delete product

--- util/database.js
+++ util/database.js
@@ -1,17 +1,29 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _dbb;
+
 const mongoConnect = callback => {
   MongoClient.connect(
-    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/test?retryWrites=true'
+    'mongodb+srv://sahithialam:<password>@cluster0.fvxqa6t.mongodb.net/?retryWrites=true&w=majority'
   )
     .then(client => {
       console.log('Connected!');
-      callback(client);
+      _db=client.db()
+      callback();
     })
     .catch(err => {
       console.log(err);
+      throw err
     });
 };
 
+const getDb =() => {
+  if(_db){
+    return _db
+  }
+  throw "no database found"
+}
+
 module.exports = mongoConnect;
+module.exports=getDb
